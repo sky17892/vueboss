7f5c099a80909e485ea1331ab324b23d362e1722
sky17892 <46701383+sky17892@users.noreply.github.com>
2024-12-25 15:10:53 +0900

    Update index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -62,7 +62,7 @@
 					  비밀번호
 				  </div>
 				  <input type="password" name="login_pw" placeholder="패스워드" class="out_login_ip" tabindex="2">				
-				  <router-link to="/indexsss"><button class="pop_login_btn" tabindex="3"><i class="fas fa-sign-in-alt" aria-hidden="true"></i> 로그인</button></router-link>
+				  <router-link to="https://vueboss.vercel.app/indexsss"><button class="pop_login_btn" tabindex="3"><i class="fas fa-sign-in-alt" aria-hidden="true"></i> 로그인</button></router-link>
 				  <span class="btn_pop_join join_btn" @click="showJoin">회원가입</span>
 				  <span class="btn_pop_tel tel_btn"><i class="fas fa-brands fa-telegram" aria-hidden="true"></i> 텔레그램 문의</span>
 			  </form>
@@ -916,7 +916,7 @@
 		  <div class="pop_join_info">
 			   정보입력
 		  </div>
-		  <form action="/customer/register_update" method="get" class="join_form">
+		  <form action="http://localhost:3000/customer/register_update" method="get" class="join_form">
 									  
 			  <div class="pop_join_sub_ttl">
 				  아이디
